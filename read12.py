abbb4a54106c9744eab3c9fea8e80e633a478ec4
D-eval <1248653135@qq.com>
2026-04-06 19:22:02 +0800

    textwise

---
+++ read12.py
@@ -0,0 +1,60 @@
+import torch
+from torch.utils.data import Dataset
+import h5py
+from pathlib import Path
+
+
+def collate_fn(batch):
+    audios = []
+    events = []
+    texts = []
+
+    for audio, event, text in batch:
+        audios.append(audio)
+        events.append(event)
+        texts.append(text)
+
+    audios = torch.stack(audios, dim=0)  # (B, T)
+    
+    return audios, events, texts
+
+
+class AudioDataset(Dataset):
+    def __init__(self, root_dir):
+        self.paths = sorted(list(Path(root_dir).glob("*.h5")))
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        h5_path = self.paths[idx]
+
+        with h5py.File(h5_path, "r") as f:
+            audio = f["audio"][:]       # (T,)
+            events = f["label/notes"][:]     # (N, 3)
+            texts = f["label/text"][()]
+        # ===== 转 tensor =====
+        audio = torch.from_numpy(audio).float()
+        events = torch.from_numpy(events).float()
+        if isinstance(texts, bytes):
+            texts = texts.decode("utf-8")
+        return audio, events, texts
+
+
+from torch.utils.data import DataLoader
+dataset = AudioDataset("../preprocess12")
+audio, events, texts = dataset[0]
+loader = DataLoader(
+    dataset,
+    batch_size=2,
+    shuffle=True,
+    # num_workers=4,
+    collate_fn=collate_fn,
+    pin_memory=True
+)
+
+for audios, events, texts in loader:
+    # audios: (B, T)
+    # targets: list[dict]
+    print("ok")
+
